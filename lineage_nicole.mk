a124a7726ee33c59d468743c7b0b364bf3e1a43d
Nolen Johnson <johnsonnolen@gmail.com>
2025-04-29 23:56:50 -0400

    nicole: We are a tablet, not a phone
    * But we do have telephony, so full_base_telephony.mk stays.
    
    Change-Id: I9e810b320a2176d4e1f6f487ca947ae86bdedc15

--- lineage_nicole.mk
+++ lineage_nicole.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/razer/nicole/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_tablet.mk)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
